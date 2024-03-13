67a58bc01bf7527001a7645355e031e4fde0a133
GabrielFerreira39 <gabrielmferreira11@gmail.com>
2024-03-13 18:06:14 -0300

    first

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../../components/searchBar/SearchBar";
+import PokemonCard from "../../components/pokemonCard/PokemonCard";
+
+export const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <PokemonCard />
+    </div>
+  );
+};
