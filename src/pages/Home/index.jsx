e6e9ee25c0a4f041d2444f43193e99d8bc3cf09f
GabrielFerreira39 <gabrielmferreira11@gmail.com>
2024-03-14 21:42:05 -0300

    Second commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,31 @@
 import NavBar from "../../components/searchBar/SearchBar";
 import PokemonCard from "../../components/pokemonCard/PokemonCard";
+import { Container } from "@mui/material";
+import { Grid } from "@mui/material";
 
 export const Home = () => {
   return (
     <div>
       <NavBar />
-      <PokemonCard />
+      <Container maxWidth="lg">
+        <Grid container>
+          <Grid item xs={2.4}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={2.4}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={2.4}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={2.4}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={2.4}>
+            <PokemonCard />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
